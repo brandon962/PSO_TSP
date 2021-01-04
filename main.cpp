98875257e1d596a185d46977c19571ec6bc6f74b
brandon962 <40123051+brandon962@users.noreply.github.com>
2021-01-04 11:55:47 +0800

    hyperpara

--- main.cpp
+++ main.cpp
@@ -4,10 +4,19 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
     cout << "?" << endl;
-    psotsp test = psotsp(30, 1000, 50, 51, 0.8, 20, 0.7, 0.3, "eil51.txt");
-    test.run();
+    if (argc < 5)
+    {
+        psotsp test = psotsp(30, 5000, 50, 51, 0.8, 40, 0.7, 0.3, "eil51.txt");
+        test.run();
+    }
+    else
+    {
+        psotsp test = psotsp(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atof(argv[5]), atoi(argv[6]), atof(argv[7]), atof(argv[8]), argv[9]);
+        test.run();
+    }
+
     return 0;
 }
